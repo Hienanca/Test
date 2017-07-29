d1a4a016f17ecbaf6ad7a95be1c9708057397317
Hien.Nguyen <Hien.Nguyen@LT-17B6M12.anca.com.au>
2017-07-29 22:46:37 +1000

    // added variable b

--- Test/src/hello.java
+++ Test/src/hello.java
@@ -4,9 +4,13 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// add
 		System.out.println("Hey, how are you?");
 		
 		int a = 5;
+		
+		// added variable b
+		int b =6;
 		
 	}
 
